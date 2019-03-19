e10c312f952f7b2572d342f8ac22ba2daf341f9e
Alin <alin.grig93@gmail.com>
2019-03-19 14:35:43 +0100

    Remove migrations and corresponding model

--- analysis/models.py
+++ analysis/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class PrincipicalComponents(models.Model):
-    run_number = models.PositiveIntegerField(unique=True, primary_key=True)
-    reconstruction = models.CharField(max_length=10)
-
-    pc1 = models.FloatField()
