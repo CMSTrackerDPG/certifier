f5604076274f6f7ea7b5b3d0ca7d7122c6ad10de
Alin <alin.grig93@gmail.com>
2019-08-21 14:03:24 +0200

    Add PCA, T-SNE and UMAP charts

--- analysis/tests/test_analysis_views.py
+++ analysis/tests/test_analysis_views.py
@@ -1,11 +1,10 @@
 import types
-
 import pytest
 from django.test import RequestFactory
 from analysis import views
 from django.urls import reverse
 
-from analysis.views import *
+import analysis.views
 
 pytestmark = pytest.mark.django_db
 
@@ -17,14 +16,14 @@ class TestChartData:
 
         assert resp.status_code == 200
 
-def test_analyse():
-    run_number = 321123
-    reco = "express"
-    arguments={'run_number': run_number, 'reco': reco }
+    def test_analyse():
+        run_number = 321123
+        reco = "express"
+        arguments={'run_number': run_number, 'reco': reco }
 
-    req = RequestFactory().get(reverse("analyse", kwargs=arguments))
-    resp = views.analyse(req, run_number, reco)
+        req = RequestFactory().get(reverse("analyse", kwargs=arguments))
+        resp = views.analyse(req, run_number, reco)
 
-    assert 200 == resp.status_code
+        assert 200 == resp.status_code
 
 
