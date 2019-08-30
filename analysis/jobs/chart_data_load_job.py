d9906bce2c1bf52f7e945c2f36e0dac6139ca2da
Alin <alin.grig93@gmail.com>
2019-08-30 09:37:35 +0200

    Change cron job implementation

--- analysis/jobs/chart_data_load_job.py
+++ analysis/jobs/chart_data_load_job.py
@@ -1,17 +1,20 @@
-from django_extensions.management.jobs import MinutelyJob
 from analysis.analyse import run_principal_component_analysis, run_tsne, run_umap, load_data
 from analysis.models import ChartDataModel
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 
-class Job(MinutelyJob):
-    help = "Chart Data Load Job"
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_data, 'interval', hours=24)
+    scheduler.start()
 
-    def execute(self):
-        runs = load_data()
+def update_data():
+    runs = load_data()
 
-        pca_data = run_principal_component_analysis(runs)
-        t_sne_data = run_tsne(runs)
-        umap_data = run_umap(runs)
+    pca_data = run_principal_component_analysis(runs)
+    t_sne_data = run_tsne(runs)
+    umap_data = run_umap(runs)
 
-        ChartDataModel.objects.create(pca_data=pca_data.to_csv(), t_sne_data=t_sne_data.to_csv(), umap_data=umap_data.to_csv())
-        import os
-        os.system("notify-send baaaaaaa")
+    ChartDataModel.objects.create(pca_data=pca_data.to_csv(), t_sne_data=t_sne_data.to_csv(), umap_data=umap_data.to_csv())
+    import os
+    os.system("notify-send baaaaaaa")
