01bd809eb8560f571a4a6b9a9579be93610af0ad
Alin <alin.grig93@gmail.com>
2019-09-26 10:41:19 +0200

    Automate reco choice and fix dataset retrieval

--- certifier/urls.py
+++ certifier/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("<int:run_number>/<reco>/", views.certify, name="certify"),
+    path("<int:run_number>/", views.certify, name="certify"),
     path("createdataset/", views.createDataset, name='createdataset'),
     path("addbadreason/", views.addBadReason, name='addbadreason'),
 ]
