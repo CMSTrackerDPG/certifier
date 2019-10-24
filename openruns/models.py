cf3d23c875bc88bfe4c123849b69ee84eef0e323
Alin <alin.grig93@gmail.com>
2019-10-24 17:23:37 +0200

    Redo of openruns(partial)

--- openruns/models.py
+++ openruns/models.py
@@ -5,10 +5,16 @@ from users.models import User
 # Create your models here.
 
 class OpenRuns(models.Model):
+    STATE_CHOICES = (
+        ("OPEN", "Open"),
+        ("COMPLETED", "Completed"),
+        ("SIGNOFF", "SignOff"),
+    )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
 
     run_number = models.PositiveIntegerField(
-        help_text="Run number", verbose_name="Run"
+        help_text="Run number", verbose_name="Run", primary_key=True
     )
 
     dataset_express = models.CharField(max_length=150)
@@ -18,8 +24,10 @@ class OpenRuns(models.Model):
 
     date_retrieved = models.DateField()
 
-    class Meta:
-        unique_together = ("run_number", "user")
+    state_express = models.CharField(max_length=9, choices=STATE_CHOICES)
+    state_prompt = models.CharField(max_length=9, choices=STATE_CHOICES)
+    state_rereco = models.CharField(max_length=9, choices=STATE_CHOICES)
+    state_rereco_ul = models.CharField(max_length=9, choices=STATE_CHOICES)
 
     def __str__(self):
         return "{}".format(self.run_number)
