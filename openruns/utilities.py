0012fc63cab4725c92dab6043d709f65ab16ec85
Alin <alin.grig93@gmail.com>
2019-10-25 11:12:13 +0200

    Get only OPEN runs now

--- openruns/utilities.py
+++ openruns/utilities.py
@@ -24,11 +24,13 @@ def get_datasets_of_runs(runs, user):
             filter={
                 'run_number': {
                     '=': run["run_number"]
-                }
+                },
+                'global_state': {
+                    '=': 'OPEN'
+                },
             }
         )
 
-        print(datasets)
         today = timezone.now().strftime("%Y-%m-%d")
 
         run_check = OpenRuns.objects.filter(run_number=run["run_number"])
@@ -41,7 +43,6 @@ def get_datasets_of_runs(runs, user):
             state_prompt=""
             state_rereco=""
             state_rereco_ul=""
-
             for dataset in datasets:
                 if "express" in dataset["name"].lower():
                     dataset_express=dataset["name"]
@@ -56,10 +57,9 @@ def get_datasets_of_runs(runs, user):
                     dataset_rereco=dataset["name"]
                     state_rereco=dataset["dataset_attributes"]["global_state"] if "global_state" in dataset["dataset_attributes"] else "SIGNOFF"
 
-            if dataset_express!="":
-                OpenRuns.objects.create(run_number=run["run_number"], dataset_express=dataset_express, user=user, state_express=state_express, date_retrieved=today)
-                OpenRuns.objects.filter(run_number=run["run_number"]).update(dataset_prompt=dataset_prompt, state_prompt=state_prompt)
-                OpenRuns.objects.filter(run_number=run["run_number"]).update(dataset_rereco=dataset_rereco, state_rereco=state_rereco)
-                OpenRuns.objects.filter(run_number=run["run_number"]).update(dataset_rereco_ul=dataset_rereco_ul, state_rereco_ul=state_rereco_ul)
+            OpenRuns.objects.create(run_number=run["run_number"], dataset_express=dataset_express, user=user, state_express=state_express, date_retrieved=today)
+            OpenRuns.objects.filter(run_number=run["run_number"]).update(dataset_prompt=dataset_prompt, state_prompt=state_prompt)
+            OpenRuns.objects.filter(run_number=run["run_number"]).update(dataset_rereco=dataset_rereco, state_rereco=state_rereco)
+            OpenRuns.objects.filter(run_number=run["run_number"]).update(dataset_rereco_ul=dataset_rereco_ul, state_rereco_ul=state_rereco_ul)
         else:
             run_check.update(date_retrieved=today)
