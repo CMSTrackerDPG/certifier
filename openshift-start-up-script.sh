27d0506290dc7505b092d2d698430c8d16191d1b
Alin <alin.grig93@gmail.com>
2020-02-11 17:24:31 +0100

    Remove migrate and makemigrations from start up script

--- openshift-start-up-script.sh
+++ openshift-start-up-script.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-python manage.py makemigrations --noinput
-
-python manage.py migrate --noinput
-
 python manage.py collectstatic --noinput
 
 daphne dqmhelper.asgi:application -u /sock/daphne.sock
