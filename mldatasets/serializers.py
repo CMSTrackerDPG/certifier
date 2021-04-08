867b56c646c5773383d98e634f121a57b4429d60
Abhit Patil <abhit95@gmail.com>
2021-04-08 12:02:55 +0200

    Add Dataset field to allRunsRefRuns API

--- mldatasets/serializers.py
+++ mldatasets/serializers.py
@@ -8,10 +8,11 @@ class RunReferenceRunSerializer(serializers.ModelSerializer):
     run_reconstruction_type = serializers.CharField(source = 'runreconstruction.reconstruction', read_only = True)
     reference_run_number = serializers.IntegerField(source = 'reference_runreconstruction.run.run_number', read_only = True)
     reference_run_reconstruction_type = serializers.CharField(source = 'reference_runreconstruction.reconstruction', read_only = True)
+    dataset = serializers.CharField(read_only = True)
 
     class Meta:
         model = TrackerCertification
-        fields = ['run_number', 'run_reconstruction_type', 'reference_run_number', 'reference_run_reconstruction_type']
+        fields = ['run_number', 'run_reconstruction_type', 'reference_run_number', 'reference_run_reconstruction_type', 'dataset']
     
     # def get_run_number(self, obj):
     #     return obj.run_number
