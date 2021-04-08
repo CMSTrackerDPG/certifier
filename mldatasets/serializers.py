fb0d096a5fc4de54199cf2f5cd36c89250b8ff14
Abhit Patil <abhit95@gmail.com>
2021-04-08 11:10:27 +0200

    Add reconstruction type for Run and Reference_Run and Replace with Serialisers Integer Field

--- mldatasets/serializers.py
+++ mldatasets/serializers.py
@@ -4,27 +4,26 @@ from oms.models import OmsRun
 
 
 class RunReferenceRunSerializer(serializers.ModelSerializer):
-    run_number = serializers.SerializerMethodField()
-    reference_run_number = serializers.SerializerMethodField()
+    run_number = serializers.IntegerField(source = 'runreconstruction.run.run_number', read_only = True)
+    run_reconstruction_type = serializers.CharField(source = 'runreconstruction.reconstruction', read_only = True)
+    reference_run_number = serializers.IntegerField(source = 'reference_runreconstruction.run.run_number', read_only = True)
+    reference_run_reconstruction_type = serializers.CharField(source = 'reference_runreconstruction.reconstruction', read_only = True)
 
     class Meta:
         model = TrackerCertification
-        fields = ['run_number', 'reference_run_number']
+        fields = ['run_number', 'run_reconstruction_type', 'reference_run_number', 'reference_run_reconstruction_type']
     
-    def get_run_number(self, obj):
-        return obj.run_number
+    # def get_run_number(self, obj):
+    #     return obj.run_number
     
-    def get_reference_run_number(self, obj):
-        return obj.reference_run_number
-
-
-class RunSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OmsRun
-        fields =  ['run_number', 'run_type']
+    # def get_reference_run_number(self, obj):
+    #     return obj.reference_run_number
 
 
 class RunReconstructionSerializer(serializers.ModelSerializer):
     class Meta:
         model = RunReconstruction
         fields =  '__all__'
+
+
+
