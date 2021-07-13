b7a9d38c380c777e9594aa790fefbebc26d76e6e
Abhit Patil <abhit95@gmail.com>
2021-07-13 16:08:39 +0200

    Add admin panel for Checklists

--- checklists/admin.py
+++ checklists/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from checklists.models import Checklist, ChecklistItem, ChecklistItemGroup
+
+admin.site.register(Checklist)
+admin.site.register(ChecklistItem)
+admin.site.register(ChecklistItemGroup)
