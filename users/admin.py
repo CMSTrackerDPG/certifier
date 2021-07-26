5f01916cfd3a2acc2714ee631905741a5cbad9f9
Abhit Patil <abhit95@gmail.com>
2021-07-26 15:21:41 +0200

    Change admin panel to custom user model instead of django user model

--- users/admin.py
+++ users/admin.py
@@ -3,5 +3,4 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 from users.models import User
 
-admin.site.register(get_user_model(), UserAdmin)
 admin.site.register(User)
