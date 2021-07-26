126baf7ac8be06402186678ae3d55b47b7fd015a
Abhit Patil <abhit95@gmail.com>
2021-07-26 16:15:50 +0200

    Modify admin panel of Custom users model

--- users/admin.py
+++ users/admin.py
@@ -3,4 +3,9 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 from users.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
