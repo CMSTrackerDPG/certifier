b8b56bebab5e6ce504107ec3c48e7f1a7335dc8f
Abhit Patil <abhit95@gmail.com>
2021-07-26 14:37:04 +0200

    Add admin panel also for custom user model which is an extension of Django user model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
+from users.models import User
 
 admin.site.register(get_user_model(), UserAdmin)
-
+admin.site.register(User)
