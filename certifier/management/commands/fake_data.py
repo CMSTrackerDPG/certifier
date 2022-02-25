5e959ae5fcf824a25b890f9c53a68d43dd9da1b6
--local <d.papag.a@gmail.com>
2022-02-25 11:05:00 +0100

    Updated fake_data command

--- certifier/management/commands/fake_data.py
+++ certifier/management/commands/fake_data.py
@@ -4,20 +4,29 @@ from . import _factories
 
 
 class Command(BaseCommand):
-    help = 'Populates the DB with fake data'
+    help = 'Populates the DB with fake data (users, tracker certification entries etc.)'
 
     def handle(self, *args, **options):
 
+        # Create 3x Pixel, Strip and Tracking problems
         pix_problems = _factories.PixelProblemFactory.create_batch(size=3)
         strip_problems = _factories.StripProblemFactory.create_batch(size=3)
         tracking_problems = _factories.TrackingProblemFactory.create_batch(
             size=3)
-        ref_trackercert = _factories.TrackerCertificationFactory.create(
-            runreconstruction__is_reference=True)
+
+        # Create a single Runreconstruction which is also a reference one
+        ref_runreconstruction = _factories.RunReconstructionFactory.create(
+            is_reference=True)
+
+        # Create 10x TrackerCertification entries.
+        # This should create all the required entries on related tables
+        # like users, OmsRuns etc.
+        # Use the previously created Runreconstruction as reference.
         test_trackercert = _factories.TrackerCertificationFactory.create_batch(
             size=10,
             pixel_problems=pix_problems,
             tracking_problems=tracking_problems,
-            strip_problems=strip_problems)
+            strip_problems=strip_problems,
+            reference_runreconstruction=ref_runreconstruction)
         self.stdout.write(
             self.style.SUCCESS(f"Created pks: {repr(test_trackercert)}"))
