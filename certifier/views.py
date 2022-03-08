4184db1f014c23bf870144caf457ddc4bfe9adf5
--local <d.papag.a@gmail.com>
2022-03-08 10:39:58 +0100

    Logging added

--- certifier/views.py
+++ certifier/views.py
@@ -1,3 +1,4 @@
+import logging
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from certifier.models import TrackerCertification, RunReconstruction, Dataset, BadReason
@@ -9,6 +10,9 @@ from oms.models import OmsRun
 from users.models import User
 from django.http import HttpResponseRedirect, Http404
 
+logger = logging.getLogger(__name__)
+
+
 @login_required
 def addBadReason(request):
     # if this is a POST request we need to process the form data
@@ -16,10 +20,12 @@ def addBadReason(request):
 
     return render(request, "certifier/badreason.html", {"form": form})
 
+
 @login_required
 def promoteToReference(request, run_number, reco):
     try:
-        runReconstruction = RunReconstruction.objects.get(run__run_number=run_number, reconstruction=reco)
+        runReconstruction = RunReconstruction.objects.get(
+            run__run_number=run_number, reconstruction=reco)
     except RunReconstruction.DoesNotExist:
         raise Http404("The run  {} doesnt exist".format(run_number))
 
@@ -28,13 +34,15 @@ def promoteToReference(request, run_number, reco):
         runReconstruction.save()
         return HttpResponseRedirect("/shiftleader/")
 
-    return render(request, "certifier/promote.html", {"runReconstruction": runReconstruction})
+    return render(request, "certifier/promote.html",
+                  {"runReconstruction": runReconstruction})
+
 
 @login_required
 def certify(request, run_number, reco=None):
-    
-    print(request)
-    
+
+    # print(request)
+    logger.debug(f"Requesting certification of run {run_number}")
     if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
         name = request.POST.get("name", None)
         dataset = request.POST.get("dataset", None)
@@ -50,30 +58,37 @@ def certify(request, run_number, reco=None):
 
             form = CertifyFormWithChecklistForm()
 
-            context = {"run_number": run_number, "reco": reco, "run": run, "dataset": dataset, "form": form}
+            context = {
+                "run_number": run_number,
+                "reco": reco,
+                "run": run,
+                "dataset": dataset,
+                "form": form
+            }
 
             return render(request, "certifier/certify.html", context)
 
-    dataset = request.GET.get('dataset',None)
+    dataset = request.GET.get('dataset', None)
 
     try:
-        
-        run = retrieve_run(run_number)
 
+        run = retrieve_run(run_number)
         if not dataset:
             if not reco:
                 dataset = retrieve_dataset(run_number)
             else:
                 dataset = retrieve_dataset_by_reco(run_number, reco)
         else:
-            dataset=dataset
-    
-        print(request)
+            dataset = dataset
+
+        # print(request)
     except (IndexError, ConnectionError) as e:
         context = {"message": "Run {} does not exist".format(run_number)}
+        logger.error(e)
         return render(request, "certifier/404.html", context)
     except Exception as e:
         context = {"message": e}
+        logger.exception(e)
         return render(request, "certifier/404.html", context)
 
     if not reco:
@@ -83,10 +98,10 @@ def certify(request, run_number, reco=None):
     if request.method == 'POST':
         try:
             runReconstruction = RunReconstruction.objects.get(
-                    run__run_number=run_number,reconstruction=reco)
+                run__run_number=run_number, reconstruction=reco)
         except RunReconstruction.DoesNotExist:
             runReconstruction = RunReconstruction.objects.create(
-                    run=run, reconstruction=reco)
+                run=run, reconstruction=reco)
 
         try:
             dataset = Dataset.objects.get(dataset=dataset)
@@ -104,12 +119,13 @@ def certify(request, run_number, reco=None):
             # ...
             # redirect to a new URL:
             try:
-                trackerCertification = TrackerCertification.objects.get(runreconstruction=runReconstruction)
+                trackerCertification = TrackerCertification.objects.get(
+                    runreconstruction=runReconstruction)
             except TrackerCertification.DoesNotExist:
                 formToSave = form.save(commit=False)
-                formToSave.runreconstruction=runReconstruction
+                formToSave.runreconstruction = runReconstruction
                 formToSave.dataset = dataset
-                formToSave.user=user
+                formToSave.user = user
                 formToSave.save()
                 form.save_m2m()
 
@@ -119,6 +135,12 @@ def certify(request, run_number, reco=None):
     if request.method == "GET":
         form = CertifyFormWithChecklistForm()
 
-    context = {"run_number": run_number, "reco": reco, "run": run, "dataset": dataset, "form": form}
+    context = {
+        "run_number": run_number,
+        "reco": reco,
+        "run": run,
+        "dataset": dataset,
+        "form": form
+    }
 
     return render(request, "certifier/certify.html", context)
