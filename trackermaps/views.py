a25c65e5c67e122afbb0b1da4f369af2fa08d5e5
--local <d.papag.a@gmail.com>
2022-03-09 12:17:38 +0100

    Fixed is_ajax (again?)

--- trackermaps/views.py
+++ trackermaps/views.py
@@ -1,40 +1,46 @@
-from django.shortcuts import render
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
-from django.conf import settings
 import subprocess
 import re
 import os
 import paramiko
+import logging
+from django.shortcuts import render
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from django.conf import settings
+
 
 def send_channel_message(group_name, message):
     channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        '{}'.format(group_name),
-        {
-            'type': 'channel_message',
-            'message': message
-        }
-    )
+    async_to_sync(channel_layer.group_send)('{}'.format(group_name), {
+        'type': 'channel_message',
+        'message': message
+    })
+
 
 def run_tracker_maps(run_type, min_run_number, max_run_number):
     #tracker_maps_command = "python /home/cctrack/run_tracker_maps.py " + str(run_type) + " " + str(min_run_number) + " " + str(max_run_number)
-    tracker_maps_command = "python /home/apatil/test_script.py " + str(run_type) + " " + str(min_run_number) + " " + str(max_run_number)
+    tracker_maps_command = "python /home/apatil/test_script.py " + str(
+        run_type) + " " + str(min_run_number) + " " + str(max_run_number)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
-    print("printing django secrets", settings.DJANGO_SECRET_ACC, settings.DJANGO_SECRET_PASS)
-   
-    ssh.connect("vocms066", username=settings.DJANGO_SECRET_ACC, password=settings.DJANGO_SECRET_PASS)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(tracker_maps_command, get_pty=True)
+
+    print("printing django secrets", settings.DJANGO_SECRET_ACC,
+          settings.DJANGO_SECRET_PASS)
+
+    ssh.connect("vocms066",
+                username=settings.DJANGO_SECRET_ACC,
+                password=settings.DJANGO_SECRET_PASS)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(tracker_maps_command,
+                                                         get_pty=True)
 
     for line in iter(ssh_stdout.readline, ""):
-        send_channel_message("output_group",line)
-    send_channel_message("output_group","GENERATION ENDED\n")
+        send_channel_message("output_group", line)
+    send_channel_message("output_group", "GENERATION ENDED\n")
 
     if ssh_stdout.channel.recv_exit_status():
         return False
     return True
+
 
 def maps(request):
     context = {}
@@ -42,18 +48,27 @@ def maps(request):
     if request.method == 'GET':
         return render(request, "trackermaps/trackermaps.html")
 
-    if request.is_ajax():
+    # is_ajax has been deprecated
+    # https://stackoverflow.com/questions/63629935/django-3-1-and-is-ajax
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
         min_run_number = request.POST.get("min", None)
         max_run_number = request.POST.get("max", None)
         run_type = request.POST.get("type", None)
         runs_list = request.POST.get("list", None)
         if runs_list:
             try:
-                runs_list = list(map(int, re.split(" , | ,|, |,| ", re.sub('\s+', ' ', runs_list).lstrip().rstrip())))
+                runs_list = list(
+                    map(
+                        int,
+                        re.split(
+                            " , | ,|, |,| ",
+                            re.sub('\s+', ' ', runs_list).lstrip().rstrip())))
             except ValueError:
-                context = {"message": "Run list should contains only numbers of runs separated by comma or space"}
+                context = {
+                    "message":
+                    "Run list should contains only numbers of runs separated by comma or space"
+                }
                 return render(request, "certifier/404.html", context)
-
 
         if min_run_number and max_run_number and not runs_list:
             run_tracker_maps(run_type, min_run_number, max_run_number)
