7b301baa7efd1bf7b85269d4e1bba4372e901a2f
--local <d.papag.a@gmail.com>
2022-03-12 12:55:53 +0100

    Fixed checklists tests

--- checklists/tests/test_checklists_models.py
+++ checklists/tests/test_checklists_models.py
@@ -1,14 +1,22 @@
 import pytest
 from mixer.backend.django import mixer
 
-from checklists.models import *
+from checklists.models import Checklist, ChecklistItem, ChecklistItemGroup
 
 pytestmark = pytest.mark.django_db
 
+
 def test_checklist():
     checklist = mixer.blend(Checklist, title="test")
-    assert "test" == str(checklist)
+    assert "test" == checklist.title
 
-def test_checklistItem():
+
+def test_checklistItemText():
     checklistItem = mixer.blend(ChecklistItem, text="test")
-    assert "test" == str(checklistItem)
+    assert "test" == checklistItem.text
+
+
+def test_checklistItemLabel():
+    checklistItem = mixer.blend(ChecklistItem, text="test")
+    assert f"{checklistItem.checklistgroup.checklist.title}/{checklistItem.checklistgroup.name}/test" == str(
+        checklistItem)
