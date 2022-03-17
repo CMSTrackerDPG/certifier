83a9a56254eacb9fe958d9878eae39507ddce523
--local <d.papag.a@gmail.com>
2022-03-17 17:03:44 +0100

    Formatting

--- shiftleader/views.py
+++ shiftleader/views.py
@@ -20,7 +20,7 @@ from shiftleader.filters import ShiftLeaderTrackerCertificationFilter
 from shiftleader.utilities.ShiftLeaderReport import ShiftLeaderReport
 from summary.utilities.SummaryReport import SummaryReport
 from checklists.models import Checklist
-# Create your views here.
+
 
 @login_required
 def shiftleader_view(request):
@@ -33,7 +33,9 @@ def shiftleader_view(request):
     """
     if request_contains_filter_parameter(request):
         return ShiftLeaderView.as_view()(request=request)
-    return HttpResponseRedirect("/shiftleader/%s" % get_this_week_filter_parameter())
+    return HttpResponseRedirect("/shiftleader/%s" %
+                                get_this_week_filter_parameter())
+
 
 # TODO lazy load summary
 @method_decorator(login_required, name="dispatch")
@@ -48,20 +50,24 @@ class ShiftLeaderView(SingleTableMixin, FilterView):
         context["summary"] = SummaryReport(self.filterset.qs)
         context["slreport"] = ShiftLeaderReport(self.filterset.qs)
         context["deleted_runs"] = DeletedTrackerCertificationTable(
-            TrackerCertification.all_objects.dead().order_by("-runreconstruction__run__run_number")
-        )
+            TrackerCertification.all_objects.dead().order_by(
+                "-runreconstruction__run__run_number"))
         try:
-            context["slchecklist"] = Checklist.objects.get(identifier="shiftleader")
+            context["slchecklist"] = Checklist.objects.get(
+                identifier="shiftleader")
         except Checklist.DoesNotExist:
             # shift leader checklist has not been created yet.
             pass
 
-        deviating, corresponding = self.filterset.qs.compare_with_run_registry()
+        deviating, corresponding = self.filterset.qs.compare_with_run_registry(
+        )
 
         if deviating:
-            context["trackercertification_comparison_table"] = RunRegistryComparisonTable(deviating)
-            context["run_registry_comparison_table"] = RunRegistryComparisonTable(
-                corresponding
-            )
+            context[
+                "trackercertification_comparison_table"] = RunRegistryComparisonTable(
+                    deviating)
+            context[
+                "run_registry_comparison_table"] = RunRegistryComparisonTable(
+                    corresponding)
 
         return context
