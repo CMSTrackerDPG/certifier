eed7d2a1125a2ab51d4222898f15270c2bf5a53f
--local <d.papag.a@gmail.com>
2022-04-05 18:30:22 +0200

    Tweaked format_integrated_luminosity so that it can output original units too

--- summary/utilities/SummaryReport.py
+++ summary/utilities/SummaryReport.py
@@ -2,6 +2,7 @@ from summary.utilities.utilities import get_ascii_table
 from listruns.utilities.luminosity import format_integrated_luminosity
 from certifier.models import TrackerCertification
 
+
 class SummaryReport:
     """
     SummaryReport used in summary.html.
@@ -20,28 +21,32 @@ class SummaryReport:
 
         for idx, runs_with_specific_type in enumerate(self.runs_per_type):
             column_description = [
-                "Run",
-                "Reference Run",
-                "Number of LS",
-                "Int. Luminosity",
-                "Pixel",
-                "Strip",
-                "Tracking",
-                "Comment"
+                "Run", "Reference Run", "Number of LS", "Int. Luminosity",
+                "Pixel", "Strip", "Tracking", "Comment"
             ]
 
             data = [[
                 run.runreconstruction.run.run_number,
                 run.reference_runreconstruction.run.run_number,
                 run.runreconstruction.run.lumisections,
-                format_integrated_luminosity(run.runreconstruction.run.recorded_lumi),
-                run.pixel,
-                run.strip,
-                run.tracking,
-                run.comment
+                format_integrated_luminosity(
+                    run.runreconstruction.run.recorded_lumi, to_ascii=True),
+                run.pixel, run.strip, run.tracking, run.comment
             ] for run in runs_with_specific_type]
 
-            headline = "Type " + str(idx + 1) + ": " + str(runs_with_specific_type[0].runreconstruction.reconstruction) + " " + str(runs_with_specific_type[0].runreconstruction.run.run_type) + " " + str(runs_with_specific_type[0].runreconstruction.run.b_field) + " T " + str(runs_with_specific_type[0].runreconstruction.run.fill_type_party1) + " " + str(runs_with_specific_type[0].runreconstruction.run.energy) + " TeV " + str(TrackerCertification.objects.get(runreconstruction=runs_with_specific_type[0]).dataset)
+            headline = "Type " + str(idx + 1) + ": " + str(
+                runs_with_specific_type[0].runreconstruction.reconstruction
+            ) + " " + str(
+                runs_with_specific_type[0].runreconstruction.run.run_type
+            ) + " " + str(
+                runs_with_specific_type[0].runreconstruction.run.b_field
+            ) + " T " + str(
+                runs_with_specific_type[0].runreconstruction.run.
+                fill_type_party1) + " " + str(
+                    runs_with_specific_type[0].runreconstruction.run.energy
+                ) + " TeV " + str(
+                    TrackerCertification.objects.get(
+                        runreconstruction=runs_with_specific_type[0]).dataset)
 
             table = get_ascii_table(column_description, data)
             runs_checked.append(headline + '\n' + table + '\n')
@@ -52,16 +57,20 @@ class SummaryReport:
         tracker_maps = []
         for idx, runs_with_specific_type in enumerate(self.runs_per_type):
             text = "Type {}".format(idx + 1)
-            tk_map_exists_runs = runs_with_specific_type.filter(trackermap="exists")
+            tk_map_exists_runs = runs_with_specific_type.filter(
+                trackermap="exists")
             if tk_map_exists_runs.exists():
                 run_numbers = tk_map_exists_runs.run_numbers()
-                joined = " ".join(str(run_number) for run_number in run_numbers)
+                joined = " ".join(
+                    str(run_number) for run_number in run_numbers)
                 text += "\n Exists: {}".format(joined)
 
-            tk_map_missing_runs = runs_with_specific_type.filter(trackermap="missing")
+            tk_map_missing_runs = runs_with_specific_type.filter(
+                trackermap="missing")
             if tk_map_missing_runs.exists():
                 run_numbers = tk_map_missing_runs.run_numbers()
-                joined = " ".join(str(run_number) for run_number in run_numbers)
+                joined = " ".join(
+                    str(run_number) for run_number in run_numbers)
                 text += "\n Missing: {}".format(joined)
             tracker_maps.append(text + "\n")
         return tracker_maps
@@ -73,13 +82,15 @@ class SummaryReport:
             good = runs_with_specific_type.good()
             if good.exists():
                 run_numbers = good.run_numbers()
-                joined = " ".join(str(run_number) for run_number in run_numbers)
+                joined = " ".join(
+                    str(run_number) for run_number in run_numbers)
                 text += "\n Good: {}".format(joined)
 
             bad = runs_with_specific_type.bad()
             if bad.exists():
                 run_numbers = bad.run_numbers()
-                joined = " ".join(str(run_number) for run_number in run_numbers)
+                joined = " ".join(
+                    str(run_number) for run_number in run_numbers)
                 text += "\n Bad: {}".format(joined)
             certified_run_numbers.append(text + "\n")
         return certified_run_numbers
@@ -88,8 +99,7 @@ class SummaryReport:
         certified_run_numbers = []
         for idx, runs_with_specific_type in enumerate(self.runs_per_type):
             column_description = [
-                "Type {}".format(idx + 1),
-                "Sum of LS",
+                "Type {}".format(idx + 1), "Sum of LS",
                 "Sum of int. luminosity"
             ]
 
@@ -98,12 +108,20 @@ class SummaryReport:
             bad = runs_with_specific_type.bad()
 
             if good.exists():
-                data.append(
-                    ["Good", good.lumisections(), format_integrated_luminosity(good.integrated_luminosity())])
+                data.append([
+                    "Good",
+                    good.lumisections(),
+                    format_integrated_luminosity(good.integrated_luminosity(),
+                                                 to_ascii=True)
+                ])
 
             if bad.exists():
-                data.append(
-                    ["Bad", bad.lumisections(), format_integrated_luminosity(bad.integrated_luminosity())])
+                data.append([
+                    "Bad",
+                    bad.lumisections(),
+                    format_integrated_luminosity(bad.integrated_luminosity(),
+                                                 to_ascii=True)
+                ])
 
             table = get_ascii_table(column_description, data)
             certified_run_numbers.append(table)
