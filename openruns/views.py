82db1508e6507c0fb4369041dbc55523cc439244
--local <d.papag.a@gmail.com>
2022-04-19 15:51:25 +0200

    Properly show warning messages in openruns page

--- openruns/views.py
+++ openruns/views.py
@@ -3,6 +3,7 @@ from django_tables2 import RequestConfig
 from django.http import HttpResponse
 from django.utils import timezone
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from cernrequests.certs import CertificateNotFound
 from urllib3.exceptions import ProtocolError
@@ -15,7 +16,7 @@ from tables.tables import OpenRunsTable
 def openruns(request):
     context = {}
 
-    if request.method == 'GET':
+    if request.method == "GET":
         run_number = request.GET.get("run_number", None)
         reco = request.GET.get("reco", None)
 
@@ -28,7 +29,7 @@ def openruns(request):
             return response
 
     # Search for openruns
-    if request.method == 'POST':
+    if request.method == "POST":
 
         min_run_number = request.POST.get("min", None)
         max_run_number = request.POST.get("max", None)
@@ -44,50 +45,47 @@ def openruns(request):
                         int,
                         re.split(
                             " , | ,|, |,| ",
-                            re.sub('\s+', ' ', runs_list).lstrip().rstrip())))
+                            re.sub("\s+", " ", runs_list).lstrip().rstrip(),
+                        ),
+                    )
+                )
             except ValueError:
                 context = {
-                    "message":
-                    "Run list should contain only numbers of runs separated by comma or space"
+                    "message": "Run list should contain only numbers of runs separated by comma or space"
                 }
                 return render(request, "certifier/404.html", context)
 
         if min_run_number and max_run_number and not runs_list:
             number_of_runs = int(max_run_number) - int(min_run_number)
             if number_of_runs >= runs_search_limit:
-                context = {
-                    "message":
-                    "Please search for less than {} runs".format(
-                        runs_search_limit)
-                }
-                #return render(request, "certifier/404.html", context)
+                messages.warning(
+                    request, f"Please search for less than {runs_search_limit} runs"
+                )
             else:
                 try:
-                    get_range_of_open_runs(min_run_number, max_run_number,
-                                           request.user)
+                    get_range_of_open_runs(min_run_number, max_run_number)
                 # Missing
                 except CertificateNotFound as e:
                     return HttpResponse(
                         "Incorrect configuration of RunRegistry certificates",
-                        status=503)
+                        status=503,
+                    )
                 except ProtocolError as e:
-                    return HttpResponse(f"ProtocolError occurred: {e}",
-                                        status=503)
+                    return HttpResponse(f"ProtocolError occurred: {e}", status=503)
 
         elif len(runs_list) >= runs_search_limit:
-            context = {
-                "message":
-                "Please search for less than {} runs".format(runs_search_limit)
-            }
-            #return render(request, "certifier/404.html", context)
+            messages.warning(
+                request, f"Please search for less than {runs_search_limit} runs"
+            )
 
         else:
             get_specific_open_runs(runs_list)
 
     today = timezone.now().strftime("%Y-%m-%d")
 
-    show_openruns = OpenRuns.objects.filter(
-        date_retrieved=today).order_by("-run_number")
+    show_openruns = OpenRuns.objects.filter(date_retrieved=today).order_by(
+        "-run_number"
+    )
 
     openruns_table = OpenRunsTable(show_openruns)
     openruns_table.request = request
