ff1df80179cb488ea785c780e1d38406bc20364e
--local <d.papag.a@gmail.com>
2022-04-26 13:05:31 +0200

    Updated skipped tests

--- shiftleader/tests/test_shiftleader_views.py
+++ shiftleader/tests/test_shiftleader_views.py
@@ -32,8 +32,8 @@ def test_view_requires_login():
 def test_view_with_request_arguments():
     req = RequestFactory().get(reverse("shiftleader:shiftleader"))
     req.GET = req.GET.copy()
-    req.GET['date__gte'] = "2019-6-10"
-    req.GET['date__lte'] = "2019-6-16"
+    req.GET["date__gte"] = "2019-6-10"
+    req.GET["date__lte"] = "2019-6-16"
 
     # Shiftleaders can access it
     req.user = mixer.blend(User, user_privilege=User.SHIFTLEADER)
@@ -46,14 +46,16 @@ def test_view_with_request_arguments():
     assert resp.status_code == 302
 
 
-@pytest.mark.skipif('TRAVIS' in os.environ,
-                    reason="skipped due to travis not being able to run it")
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" in os.environ,
+    reason="skipped due to GitHub actions not being able to run it",
+)
 def test_view_compare_with_run_registry():
     create_runs(3, 1, "collisions", "express", date="2018-05-14")
     req = RequestFactory().get("shiftleader/")
     req.GET = req.GET.copy()
-    req.GET['date__gte'] = "2018-5-13"
-    req.GET['date__lte'] = "2018-5-16"
+    req.GET["date__gte"] = "2018-5-13"
+    req.GET["date__lte"] = "2018-5-16"
     req.user = mixer.blend(User, user_privilege=User.SHIFTLEADER)
     resp = shiftleader_view(req)
     assert resp.status_code == 200
