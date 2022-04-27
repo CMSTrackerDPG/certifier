961b82aad53a856bd3361e968f8ad6269189c2d5
--local <nothingface0@gmail.com>
2022-04-27 16:04:17 +0200

    Formatting

--- trackermaps/views.py
+++ trackermaps/views.py
@@ -28,16 +28,16 @@ done
 # """
 
 
-def send_channel_message(channel_layer: RedisChannelLayer, group_name: str,
-                         message: str) -> None:
+def send_channel_message(
+    channel_layer: RedisChannelLayer, group_name: str, message: str
+) -> None:
     """
     Function that sends a message
     """
 
-    async_to_sync(channel_layer.group_send)(group_name, {
-        'type': 'channel_message',
-        'message': message
-    })
+    async_to_sync(channel_layer.group_send)(
+        group_name, {"type": "channel_message", "message": message}
+    )
 
 
 def run_tracker_maps(run_type: str, run_number_list: list) -> bool:
@@ -45,30 +45,36 @@ def run_tracker_maps(run_type: str, run_number_list: list) -> bool:
     Function that connects to vocms066 using the env-supplied username/password
     and executes the script to generate specific tracker maps.
     """
-    tracker_maps_command = "cd /data/users/event_display/ShiftRun3/TkMapGeneration/CMS* &&"\
-    " bash /data/users/event_display/ShiftRun3/TkMapGeneration/tkmapsFromCertHelper.sh"\
-    f" {str(run_type)} {''.join(str(run_number)+' ' for run_number in run_number_list)}"
+    tracker_maps_command = (
+        "cd /data/users/event_display/ShiftRun3/TkMapGeneration/CMS* &&"
+        " bash /data/users/event_display/ShiftRun3/TkMapGeneration/tkmapsFromCertHelper.sh"
+        f" {str(run_type)} {''.join(str(run_number)+' ' for run_number in run_number_list)}"
+    )
     channel_layer = get_channel_layer()
 
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    send_channel_message(channel_layer, "output_group",
-                         "-------- CONNECTING TO vocms066 --------\n")
+    send_channel_message(
+        channel_layer, "output_group", "-------- CONNECTING TO vocms066 --------\n"
+    )
     try:
-        ssh.connect("vocms066",
-                    username=settings.DJANGO_SECRET_ACC,
-                    password=settings.DJANGO_SECRET_PASS)
+        ssh.connect(
+            "vocms066",
+            username=settings.DJANGO_SECRET_ACC,
+            password=settings.DJANGO_SECRET_PASS,
+        )
     except Exception as e:
         logger.exception(e)
         send_channel_message(channel_layer, "output_group", repr(e))
         raise
-    send_channel_message(channel_layer, "output_group",
-                         "-------- CONNECTED --------\n")
-    send_channel_message(channel_layer, "output_group",
-                         "-------- SCRIPT STARTED --------\n")
+    send_channel_message(channel_layer, "output_group", "-------- CONNECTED --------\n")
+    send_channel_message(
+        channel_layer, "output_group", "-------- SCRIPT STARTED --------\n"
+    )
     logger.debug(f"Executing '{tracker_maps_command}'")
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(tracker_maps_command,
-                                                         get_pty=True)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
+        tracker_maps_command, get_pty=True
+    )
 
     # ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(TEST_BASH_SCRIPT,
     #                                                      get_pty=True)
@@ -87,11 +93,12 @@ def run_tracker_maps(run_type: str, run_number_list: list) -> bool:
         logger.debug(line)
         send_channel_message(channel_layer, "output_group", f"{line}")
 
-    send_channel_message(channel_layer, "output_group",
-                         "-------- SCRIPT STOPPED --------\n")
+    send_channel_message(
+        channel_layer, "output_group", "-------- SCRIPT STOPPED --------\n"
+    )
 
     for line in line_buffered(ssh_stdin):
-        line = line.decode('utf-8', errors='ignore')  # Convert bytes to str
+        line = line.decode("utf-8", errors="ignore")  # Convert bytes to str
         send_channel_message(channel_layer, "output_group", f"{line}")
 
     # Verify script's exit status
@@ -103,9 +110,11 @@ def run_tracker_maps(run_type: str, run_number_list: list) -> bool:
     return True
 
 
-@user_passes_test(lambda user: hasattr(user, 'has_shift_leader_rights') and
-                  user.has_shift_leader_rights,
-                  redirect_field_name=None)
+@user_passes_test(
+    lambda user: hasattr(user, "has_shift_leader_rights")
+    and user.has_shift_leader_rights,
+    redirect_field_name=None,
+)
 def maps(request):
     """
     View for the trackermaps/ url
@@ -114,12 +123,13 @@ def maps(request):
 
     # is_ajax has been deprecated
     # https://stackoverflow.com/questions/63629935/django-3-1-and-is-ajax
-    if request.method == 'POST' and request.META.get(
-            'HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+    if (
+        request.method == "POST"
+        and request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
+    ):
         run_type = request.POST.get("type", None)
         runs_list = request.POST.get("list", None)
-        logger.info(
-            f"Got trackermaps generation request for {run_type}: {runs_list}")
+        logger.info(f"Got trackermaps generation request for {run_type}: {runs_list}")
         if runs_list:
             try:
                 runs_list = list(
@@ -127,12 +137,14 @@ def maps(request):
                         int,
                         re.split(
                             " , | ,|, |,| ",
-                            re.sub(r'\s+', ' ', runs_list).lstrip().rstrip())))
+                            re.sub(r"\s+", " ", runs_list).lstrip().rstrip(),
+                        ),
+                    )
+                )
                 logger.debug(f"Parsed runs list: {runs_list}")
             except ValueError:
                 context = {
-                    "message":
-                    "Run list should contain only numbers of runs separated by comma or space"
+                    "message": "Run list should contain only numbers of runs separated by comma or space"
                 }
                 return JsonResponse(context, status=400)
             except Exception as e:
@@ -140,7 +152,6 @@ def maps(request):
                 return JsonResponse(context, status=500)
 
         # Start on a separate thread
-        threading.Thread(target=run_tracker_maps,
-                         args=(run_type, runs_list)).start()
+        threading.Thread(target=run_tracker_maps, args=(run_type, runs_list)).start()
 
     return render(request, "trackermaps/trackermaps.html")
