f067fbba0bc8a91b0aa281c412725839343b922e
--local <nothingface0@gmail.com>
2022-05-04 16:50:02 +0200

    Cleanup, revert to old var name

--- certifier/urls.py
+++ certifier/urls.py
@@ -4,12 +4,8 @@ from certifier import views
 urlpatterns = [
     re_path("<int:run_number>/(?P<dataset>.+)/$", views.certify, name="certify"),
     path("<int:run_number>/", views.certify, name="certify"),
-    path("<int:run_number>/<reconstruction_type>/", views.certify, name="certify"),
-    path(
-        "promote/<int:run_number>/<reconstruction_type>/",
-        views.promoteToReference,
-        name="promote",
-    ),
-    path("addbadreason/", views.addBadReason, name="addbadreason"),
+    path("<int:run_number>/<reco>/", views.certify, name="certify"),
+    path("promote/<int:run_number>/<reco>/", views.promoteToReference, name="promote",),
+    path("addbadreasonform/", views.addBadReason, name="addbadreasonform"),
     path("allRunsRefRuns/", views.runRefRun_list),
 ]
--- certifier/views.py
+++ certifier/views.py
@@ -54,17 +54,15 @@ def promoteToReference(request, run_number, reco):
 
 
 @login_required
-def certify(request, run_number, reconstruction_type=None):
+def certify(request, run_number, reco=None):
 
-    logger.debug(
-        f"Requesting certification of run {run_number} {reconstruction_type if reconstruction_type else ''}"
-    )
+    logger.debug(f"Requesting certification of run {run_number} {reco if reco else ''}")
 
     # Check if specific combination can be certified by current user
     if not TrackerCertification.can_be_certified_by_user(
-        run_number, reconstruction_type, request.user
+        run_number, reco, request.user
     ):
-        msg = f"Reconstruction {run_number} {reconstruction_type} is already certified by another user"
+        msg = f"Reconstruction {run_number} {reco} is already certified by another user"
         logger.warning(msg)
         return render(
             request,
@@ -73,15 +71,40 @@ def certify(request, run_number, reconstruction_type=None):
             status=400,
         )
 
+    # Add bad reason
+    if request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest":
+        name = request.POST.get("name", None)
+        dataset = request.POST.get("dataset", None)
+        description = request.POST.get("description", None)
+        run = OmsRun.objects.get(run_number=run_number)
+
+        if name and description and dataset:
+            try:
+                BadReason.objects.get(name=name)
+            except BadReason.DoesNotExist:
+                BadReason.objects.create(name=name, description=description)
+
+            form = CertifyFormWithChecklistForm()
+
+            context = {
+                "run_number": run_number,
+                "reco": reco,
+                "run": run,
+                "dataset": dataset,
+                "form": form,
+            }
+
+            return render(request, "certifier/certify.html", context)
+
     dataset = request.GET.get("dataset", None)
     run = None
     try:
         run = retrieve_run(run_number)
         if not dataset:
-            if not reconstruction_type:
+            if not reco:
                 dataset = retrieve_dataset(run_number)
             else:
-                dataset = retrieve_dataset_by_reco(run_number, reconstruction_type)
+                dataset = retrieve_dataset_by_reco(run_number, reco)
 
     except IndexError as e:
         context = {"message": f"Run {run_number} does not exist"}
@@ -97,7 +120,7 @@ def certify(request, run_number, reconstruction_type=None):
 
         # If no reconstruction is specified and there's no connection
         # to RR, we cannot get the next available reconstruction type & dataset
-        if not reconstruction_type:
+        if not reco:
             context = {
                 "message": f"Cannot proceed with certification if no reconstuction type is specified ({e})",
                 "error_num": 400,
@@ -112,47 +135,22 @@ def certify(request, run_number, reconstruction_type=None):
         logger.exception(repr(e))
         return render(request, "certifier/http_error.html", context, status=500)
 
-    if not reconstruction_type:
+    if not reco:
         if dataset:
-            reconstruction_type = get_reco_from_dataset(dataset)
+            reco = get_reco_from_dataset(dataset)
         else:
-            reconstruction_type = None
+            reco = None
 
     # if this is a POST request we need to process the form data
     if request.method == "POST":
 
-        # Add bad reason
-        if request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest":
-            name = request.POST.get("name", None)
-            dataset = request.POST.get("dataset", None)
-            description = request.POST.get("description", None)
-            run = OmsRun.objects.get(run_number=run_number)
-
-            if name and description and dataset:
-                try:
-                    BadReason.objects.get(name=name)
-                except BadReason.DoesNotExist:
-                    BadReason.objects.create(name=name, description=description)
-
-                form = CertifyFormWithChecklistForm()
-
-                context = {
-                    "run_number": run_number,
-                    "reconstruction_type": reconstruction_type,
-                    "run": run,
-                    "dataset": dataset,
-                    "form": form,
-                }
-
-                return render(request, "certifier/certify.html", context)
-
         try:
             runReconstruction = RunReconstruction.objects.get(
-                run__run_number=run_number, reconstruction=reconstruction_type
+                run__run_number=run_number, reconstruction=reco
             )
         except RunReconstruction.DoesNotExist:
             runReconstruction = RunReconstruction.objects.create(
-                run=run, reconstruction=reconstruction_type
+                run=run, reconstruction=reco
             )
 
         dataset, _ = Dataset.objects.get_or_create(dataset=dataset)
@@ -197,7 +195,7 @@ def certify(request, run_number, reconstruction_type=None):
 
     context = {
         "run_number": run_number,
-        "reconstruction_type": reconstruction_type,
+        "reco": reco,
         "run": run,
         "dataset": dataset,
         "form": form,
