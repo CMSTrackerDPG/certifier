0864d9682da0c2781e944a7f78821974c611a1f7
--local <nothingface0@gmail.com>
2022-05-25 18:07:01 +0200

    Added test for helper function

--- oms/tests/test_oms_utils.py
+++ oms/tests/test_oms_utils.py
@@ -4,7 +4,7 @@ import pytest
 # from django.test import RequestFactory
 # from mixer.backend.django import mixer
 from oms.models import OmsFill, OmsRun
-from oms.utils import oms_retrieve_fill, oms_retrieve_run
+from oms.utils import oms_retrieve_fill, oms_retrieve_run, get_reco_from_dataset
 
 pytestmark = pytest.mark.django_db
 
@@ -24,3 +24,14 @@ def test_oms_retrieve_run():
     oms_retrieve_run(run_number)
 
     assert OmsRun.objects.get(run_number=run_number)
+
+
+def test_reco_from_dataset():
+    test_cases = [
+        ("express", "/StreamExpressCosmics/Commissioning2018-Express-v1/DQMIO"),
+        ("prompt", "/Cosmics/Commissioning2018-PromptReco-v1/DQMIO"),
+        ("rereco", "/ReReco/Run2018A_17Sept2018/DQM"),
+        ("rerecoul", "/ReReco/Run2017B_UL2019/DQM"),
+    ]
+    for case in test_cases:
+        assert get_reco_from_dataset(case[1]) == case[0]
