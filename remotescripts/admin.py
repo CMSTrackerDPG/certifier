789687198bbaaa739a033f71a78dee5497b79f2b
--local <nothingface0@gmail.com>
2022-05-26 13:51:36 +0200

    Args and Kwargs stored in DB work

--- remotescripts/admin.py
+++ remotescripts/admin.py
@@ -1,3 +1,4 @@
+import logging
 from django.contrib import admin
 from remotescripts.models import (
     ScriptConfigurationBase,
@@ -8,8 +9,16 @@ from remotescripts.models import (
     RemoteScriptConfiguration,
 )
 
+logger = logging.getLogger(__name__)
+
 admin.site.register(BashScriptConfiguration)
-admin.site.register(RemoteScriptConfiguration)
+
+
+@admin.action(description="Execute this script")
+def execute(modeladmin, request, queryset):
+    for s in queryset:
+        logger.info(f"Executing {str(s)}")
+        s.execute()
 
 
 @admin.register(ScriptKeywordArgument)
@@ -34,3 +43,18 @@ class ScriptOutputFileAdmin(admin.ModelAdmin):
         if db_field.name == "mother_script":
             kwargs["queryset"] = ScriptConfigurationBase.objects.select_subclasses()
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(RemoteScriptConfiguration)
+class RemoteScriptConfigurationAdmin(admin.ModelAdmin):
+    list_display = ("title", "num_pos_args", "num_kw_args", "num_output_files")
+    actions = [execute]
+
+    def num_pos_args(self, obj):
+        return obj.positional_arguments.count()
+
+    def num_kw_args(self, obj):
+        return obj.keyword_arguments.count()
+
+    def num_output_files(self, obj):
+        return obj.output_files.count()
--- remotescripts/models.py
+++ remotescripts/models.py
@@ -5,7 +5,7 @@ import subprocess
 import paramiko
 from django.db import models
 from django.conf import settings
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from remotescripts.validators import validate_bash_script
 from model_utils.managers import InheritanceManager
 
@@ -13,9 +13,14 @@ logger = logging.getLogger(__name__)
 
 
 class ScriptConfigurationBase(models.Model):
+    """
+    Base model for configuration common across different
+    type of scripts to execute
+    """
+
     objects = InheritanceManager()
     title = models.CharField(
-        max_length=20, help_text="Script title to display", null=True
+        max_length=50, help_text="Script title to display", null=True
     )
 
     base_command = models.TextField(
@@ -32,13 +37,29 @@ class ScriptConfigurationBase(models.Model):
 
     def _form_command(self, *args, **kwargs) -> str:
         cmd = str(self.base_command)
-        print(type(cmd), cmd)
-        if self.positional_arguments.count() > 0:
-            pass
+        num_pos_args = self.positional_arguments.count()
+        num_kw_args = self.keyword_arguments.count()
+        if num_pos_args > 0:
+            if len(args) != num_pos_args:
+                raise TypeError(
+                    f"Script requires {num_pos_args} positional argument(s) but {len(args)} was given"
+                )
+            p = self.positional_arguments.filter().order_by("position")
 
-        if self.keyword_arguments.count() > 0:
-            k = self.keyword_arguments
-            print(k)
+            for i, _p in enumerate(p):
+                # TODO Typecheck
+                cmd += f" {str(args[i])}"
+
+        if num_kw_args > 0:
+            if len(kwargs) != num_kw_args:
+                raise TypeError(
+                    f"Script requires {num_kw_args} keyword argument(s) but {len(kwargs)} was given"
+                )
+            k = self.keyword_arguments.all()
+            for _k in k:
+                if _k.keyword not in kwargs:
+                    raise TypeError(f"Missing keyword argument '{_k.keyword}'")
+                cmd += f" {str(_k)}{kwargs[_k.keyword]}"
         return cmd
 
     def save(self, *args, **kwargs):
@@ -51,7 +72,17 @@ class ScriptConfigurationBase(models.Model):
 
 
 class BashScriptConfiguration(ScriptConfigurationBase):
+    """
+    Model for local bash script configuration
+    """
+
     def execute(self, *args, **kwargs):
+        """
+        The idea here is to save the script in a temp file,
+        then pass it to bash via subprocess.
+
+        Not tested a lot, but the main idea works.
+        """
         cmd_to_execute = self._form_command(*args, **kwargs)
         with tempfile.NamedTemporaryFile() as fp:
             fp.write(cmd_to_execute.encode())
@@ -110,6 +141,8 @@ class RemoteScriptConfiguration(ScriptConfigurationBase):
         """
         Method that executes remote script
         """
+        cmd_to_execute = self._form_command(*args, **kwargs)
+
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(
@@ -118,7 +151,6 @@ class RemoteScriptConfiguration(ScriptConfigurationBase):
             password=getattr(settings, self.env_secret_password),
             port=self.port,
         )
-        cmd_to_execute = self._form_command(*args, **kwargs)
         logger.debug(f"Executing '{cmd_to_execute}'")
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(
             cmd_to_execute, get_pty=True
@@ -167,12 +199,19 @@ class ScriptArgumentBase(models.Model):
     ARGUMENT_INT = "INT"
     ARGUMENT_STR = "STR"
     ARGUMENT_CHOICES = ((ARGUMENT_INT, "Integer"), (ARGUMENT_STR, "String"))
-    argument_type = models.CharField(choices=ARGUMENT_CHOICES, max_length=3)
+    name = models.CharField(max_length=20, null=True, default="")
+    type = models.CharField(
+        max_length=3, choices=ARGUMENT_CHOICES, default=ARGUMENT_STR
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
 
 
 class ScriptPositionalArgument(ScriptArgumentBase):
-    position = models.PositiveIntegerField(
-        help_text="Position to be placed after command. 0 means don't care"
+    position = models.PositiveSmallIntegerField(
+        help_text="Position to be placed after command.",
+        validators=[MinValueValidator(limit_value=1)],
     )
     mother_script = models.ForeignKey(
         ScriptConfigurationBase,
@@ -182,8 +221,14 @@ class ScriptPositionalArgument(ScriptArgumentBase):
         related_name="positional_arguments",
     )
 
+    class Meta:
+        unique_together = [["position", "mother_script"]]
+
     def __str__(self):
-        return f"Positional argument (Pos:{self.position}) for {ScriptConfigurationBase.objects.get_subclass(id=self.mother_script.pk)}"
+        return (
+            f"{self.name} (Pos:{self.position}) for "
+            f"{ScriptConfigurationBase.objects.get_subclass(id=self.mother_script.pk)}"
+        )
 
 
 class ScriptKeywordArgument(ScriptArgumentBase):
@@ -191,7 +236,10 @@ class ScriptKeywordArgument(ScriptArgumentBase):
     SEPARATOR_EQUALS = "="
     SEPARATOR_CHOICES = ((SEPARATOR_SPACE, "Space"), (SEPARATOR_EQUALS, "="))
     keyword = models.CharField(
-        max_length=50, help_text="Keyword name for this argument", null=False
+        max_length=50,
+        help_text="Keyword name for this argument",
+        null=False,
+        default="keyword",
     )
     separator = models.CharField(
         max_length=2,
