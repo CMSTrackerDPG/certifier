0a967425d8894de2b9a78d99552132ffeb958314
--local <nothingface0@gmail.com>
2022-05-26 12:40:48 +0200

    Made dropdowns in admin site more intuitive

--- remotescripts/admin.py
+++ remotescripts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from remotescripts.models import (
+    ScriptConfigurationBase,
     BashScriptConfiguration,
     ScriptKeywordArgument,
     ScriptPositionalArgument,
@@ -8,7 +9,28 @@ from remotescripts.models import (
 )
 
 admin.site.register(BashScriptConfiguration)
-admin.site.register(ScriptKeywordArgument)
-admin.site.register(ScriptPositionalArgument)
-admin.site.register(ScriptOutputFile)
 admin.site.register(RemoteScriptConfiguration)
+
+
+@admin.register(ScriptKeywordArgument)
+class ScriptKeywordArgumentAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "mother_script":
+            kwargs["queryset"] = ScriptConfigurationBase.objects.select_subclasses()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(ScriptPositionalArgument)
+class ScriptPositionalArgumentAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "mother_script":
+            kwargs["queryset"] = ScriptConfigurationBase.objects.select_subclasses()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(ScriptOutputFile)
+class ScriptOutputFileAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "mother_script":
+            kwargs["queryset"] = ScriptConfigurationBase.objects.select_subclasses()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
