ee4d20f18433821b90dcb9d0f9e02299ea814a92
--local <nothingface0@gmail.com>
2022-07-07 14:26:31 +0200

    Verify that invalid run nums do not crash update refruns info

--- addrefrun/tests/test_addrefrun_views.py
+++ addrefrun/tests/test_addrefrun_views.py
@@ -145,3 +145,25 @@ class TestUpdateReferenceReconstructionRunsInfo:
         # Response is a JSON with a "success" key
         data = json.loads(resp.content.decode("utf-8"))
         assert data["success"] is True
+
+    def test_update_refruns_info_invalid(self):
+        """
+        Create a reference runreconstruction and a run
+        corresponding to it, and try the update_refruns_info endpoint
+        """
+        run_numbers = [321123, 999999]
+        for run_number in run_numbers:
+            mixer.blend(
+                RunReconstruction,
+                reconstruction=RunReconstruction.EXPRESS,
+                run=mixer.blend(OmsRun, run_number=run_number),
+                is_reference=True,
+            )
+
+        req = RequestFactory().get(reverse("addrefrun:update_refruns_info"))
+        req.user = mixer.blend(User, user_privilege=User.SHIFTLEADER)
+        resp = views.update_refruns_info(req)
+
+        # Response is a JSON with a "success" key
+        data = json.loads(resp.content.decode("utf-8"))
+        assert data["success"] is True
