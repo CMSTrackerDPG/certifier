7fbe38945016590b8aeaa4e88816ad91868f4da2
--local <nothingface0@gmail.com>
2022-07-08 12:20:06 +0200

    UpdateView for OmsFill

--- oms/views.py
+++ oms/views.py
@@ -6,3 +6,9 @@ class OmsRunUpdateView(UpdateView):
     model = OmsRun
     template_name = "oms/omsrun_form.html"
     fields = ["run_type", "fill"]
+
+
+class OmsFillUpdateView(UpdateView):
+    model = OmsFill
+    template_name = "oms/omsfill_form.html"
+    fields = ["fill_number"]
