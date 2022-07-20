44ebc5ece31633f3f261f9325898ffa1caef8d59
--local <nothingface0@gmail.com>
2022-07-20 13:15:49 +0200

    Convert luminosity to 1/pb always for now

--- listruns/utilities/luminosity.py
+++ listruns/utilities/luminosity.py
@@ -1,10 +1,21 @@
 from decimal import Decimal
 from listruns.utilities.manip import strip_trailing_zeros
 
+CONVERSION_MAP = {
+    r"pb^{-1}": {"ascii": "/pb", "default": "pb⁻¹", "factor": 1},
+    r"{\mu}b^{-1}": {"ascii": "/ub", "default": "µb⁻¹", "factor": 1e-6},
+}
+
+
+def convert_luminosity_to_pb(
+    int_luminosity: float = None, luminosity_units: str = "pb^{-1}"
+) -> float:
+    return float(int_luminosity * CONVERSION_MAP[luminosity_units]["factor"])
+
 
 def format_integrated_luminosity(
     int_luminosity: float = None, luminosity_units: str = "pb^{-1}", to_ascii=False
-):
+) -> float:
     """
     Example:
     >>> format_integrated_luminosity(Decimal("0.000000266922"))
@@ -16,30 +27,25 @@ def format_integrated_luminosity(
     :param luminosity_units: units luminosity is counted in
     :return: Formatted luminosity with 3 decimal points precision
     """
-    CONVERSION_MAP = {
-        r"pb^{-1}": {"ascii": "/pb", "default": "pb⁻¹", "exponent": 12},
-        r"{\mu}b^{-1}": {"ascii": "/ub", "default": "µb⁻¹", "exponent": 6},
-    }
+
     if int_luminosity is None:
         int_luminosity = 0
 
     formatted_luminosity = ""
-    value = Decimal(int_luminosity)
 
+    value = convert_luminosity_to_pb(int_luminosity, luminosity_units)
     # Convert /pb to /ub if very small /pb value
-    if f"{value:.3f}" == "0.000" and CONVERSION_MAP[luminosity_units]["exponent"] == 12:
-        value = Decimal("1E6") * value
-        value_string = f"{value:.3f}"
-        formatted_value = strip_trailing_zeros(value_string)
-        formatted_units = CONVERSION_MAP[r"{\mu}b^{-1}"][
-            "ascii" if to_ascii else "default"
-        ]
-    else:
-        value_string = f"{value:.3f}"
-        formatted_value = strip_trailing_zeros(value_string)
-        formatted_units = CONVERSION_MAP[luminosity_units][
-            "ascii" if to_ascii else "default"
-        ]
+    # if f"{value:.3f}" == "0.000":
+    # value = Decimal("1E6") * value
+    #     value_string = f"{value:.3f}"
+    #     formatted_value = strip_trailing_zeros(value_string)
+    #     formatted_units = CONVERSION_MAP[r"{\mu}b^{-1}"][
+    #         "ascii" if to_ascii else "default"
+    #     ]
+    # else:
+    value_string = f"{value:.3f}"
+    formatted_value = strip_trailing_zeros(value_string)
+    formatted_units = CONVERSION_MAP["pb^{-1}"]["ascii" if to_ascii else "default"]
 
     formatted_luminosity = f"{formatted_value} {formatted_units}"
     return formatted_luminosity
