2c8c79156cfd81c3019062a6c2dba0b2f5755923
--local <nothingface0@gmail.com>
2022-07-27 17:34:19 +0200

    Fixed management command

--- shiftleader/management/commands/generate_presentation.py
+++ shiftleader/management/commands/generate_presentation.py
@@ -1,23 +1,40 @@
+from datetime import date, timedelta
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from shiftleader.utilities.shiftleader_report_presentation import (
     ShiftLeaderReportPresentation,
 )
+from certifier.models import TrackerCertification
 
 
 class Command(BaseCommand):
     help = ""
 
     def add_arguments(self, parser):
+        parser.add_argument("year", type=int)
         parser.add_argument("week_number", type=int)
 
     def handle(self, *args, **options):
+        year = options["year"]
+        week_number = options["week_number"]
+
+        # Get first day of week requested
+        week_start = date.fromisocalendar(year=year, week=week_number, day=1)
+
+        week_end = week_start + timedelta(days=6)
+
+        queryset = TrackerCertification.objects.filter(
+            date__gte=week_start,
+            date__lte=week_end,
+        )
         p = ShiftLeaderReportPresentation(
             requesting_user="",
             certhelper_version=settings.CERTHELPER_VERSION,
-            week_number=options["week_number"],
+            year=year,
+            week_number=week_number,
             name_shift_leader="",
             names_shifters=[],
             names_oncall=[],
+            certification_queryset=queryset,
         )
         p.save()
