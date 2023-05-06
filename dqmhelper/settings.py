bcf99f0dbe4e8af7445a4fea3ebf2c8125920039
Dimitris Papagiannis <d.papag.a@gmail.com>
2023-05-06 13:09:49 +0200

    Fixed redis config string

--- dqmhelper/settings.py
+++ dqmhelper/settings.py
@@ -132,7 +132,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("redis://:" + REDIS_PASSWORD + "@" + REDIS_HOST + ":6379/0")],
         },
     },
 }
