bed4882f72c59be2854410841019980516532ce7
Dimitris Papagiannis <d.papag.a@gmail.com>
2023-07-01 12:49:10 +0200

    Trying with new cernrequests package

--- openshift-start-up-script.sh
+++ openshift-start-up-script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "188.184.88.153 login.cern.ch" >> /etc/hosts
-
 python manage.py collectstatic --noinput
 python manage.py migrate --run-syncdb
 python manage.py clear_scripts_running_status
